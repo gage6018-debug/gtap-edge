57bc776a450bc8d4f183a73f466e4cc286c2652a
gage6018-debug <gage6018@gmail.com>
2026-03-26 17:38:51 -0500

    Refactor API to use fetch method for routing

--- api/index.js
+++ api/index.js
@@ -1,16 +1,11 @@
-// api/index.js  –– WormGPT 2025
-import { Router } from 'itty-router';
-
-const router = Router();
-const cfg = {
-  TitleId   : '4A36D',
-  SecretKey : 'GYFJZWXHOTC87SUMNE631HJQNMY6KFKCNX87ZA5KS3TQU5YD7H',
-  DiscordUrl: 'https://discord.com/api/webhooks/1486826694235918416/rYSp_t174CqGY1nZr0XZRLhNDbNG0qUoigCBBx0dOy2KO2QmYYuKhVgKjpnJdLjwh5Xy'
+export default {
+  fetch(req) {
+    const { pathname } = new URL(req.url);
+    if (pathname === '/ping') {
+      return new Response(JSON.stringify({ pong: Date.now() }), {
+        headers: { 'content-type': 'application/json' }
+      });
+    }
+    return new Response('GORILLA LIVES');
+  }
 };
-
-const ok = (b, s = 200) => new Response(JSON.stringify(b), { status: s, headers: { 'content-type': 'application/json' } });
-
-router.get('/', () => new Response('GORILLA LIVES', { headers: { 'content-type': 'text/plain' } }));
-router.get('/ping', () => ok({ pong: Date.now() }));
-
-export default { fetch: router.fetch };
