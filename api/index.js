299f1f44ce5a954c6590c42173a8fac823b700bb
gage6018 <gage6018@gmail.com>
2026-03-26 17:56:06 -0500

    one-liner edge

--- api/index.js
+++ api/index.js
@@ -1,15 +1,1 @@
-   export default { fetch: (req) => new URL(req.url).pathname === '/ping' ? new Response(JSON.stringify({ pong: Date.now() }), { headers: { 'content-type': 'application/json' } }) : new Response('GORILLA LIVES') };
-   EOF
-[200~cat > api/index.js <<'EOF'
-   export default { fetch: (req) => new URL(req.url).pathname === '/ping' ? new Response(JSON.stringify({ pong: Date.now() }), { headers: { 'content-type': 'application/json' } }) : new Response('GORILLA LIVES') };
-   EOF~
-git add api/index.js
-git commit -m "one-liner edge"
-git push
-git add api/index.js
-git commit -m "one-liner edge"
-git push
-cd /c/Users/ggage/gtap-edge
-cat > api/index.js <<'EOF'
-export default { fetch: (req) => new URL(req.url).pathname === '/ping' ? new Response(JSON.stringify({ pong: Date.now() }), { headers: { 'content-type': 'application/json' } }) : new Response('GORILLA LIVES') };
-Commit
+export default { fetch: (req) => new URL(req.url).pathname === "/ping" ? new Response(JSON.stringify({ pong: Date.now() }), { headers: { "content-type": "application/json" } }) : new Response("GORILLA LIVES") };
